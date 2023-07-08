c77f9134368dbce13c0d92c49aa4e5c2e04228f8
pink1391 <80207490+pink1391@users.noreply.github.com>
2023-07-09 00:52:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/student',(req,res)=>{
-    res.send("This is student khittu pagla");
+    res.send("This is student khittu pagla1");
 })
 
 app.get('/test',(req,res)=>{
